b82d1573f98c20bc76873e89cec9c1f6f1a0b4c5
liyajun <332209078@qq.com>
2022-09-28 20:34:31 +0800

    完成教师管理

--- src/main/java/com/wis/school/service/impl/TeacherServiceImpl.java
+++ src/main/java/com/wis/school/service/impl/TeacherServiceImpl.java
@@ -7,12 +7,15 @@
 package com.wis.school.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.wis.school.mapper.TeacherMapper;
 import com.wis.school.pojo.LoginForm;
 import com.wis.school.pojo.Teacher;
 import com.wis.school.service.TeacherService;
 import com.wis.school.util.Encode_MD5;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -44,5 +47,17 @@ public class TeacherServiceImpl extends ServiceImpl<TeacherMapper, Teacher> impl
         QueryWrapper<Teacher> queryWrapper = new QueryWrapper<>();
         queryWrapper.eq("id", userId);
         return baseMapper.selectOne(queryWrapper);
+    }
+
+    @Override
+    public IPage<Teacher> getTeachers(Page<Teacher> page, String name) {
+        QueryWrapper<Teacher> queryWrapper = new QueryWrapper<>();
+        if (!StringUtils.isEmpty(name)) {
+            queryWrapper.like("name", name);
+        }
+        queryWrapper.orderByDesc("id");
+        queryWrapper.orderByAsc("name");
+
+        return baseMapper.selectPage(page, queryWrapper);
     }
 }
